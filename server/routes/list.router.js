48df3a4cfd1c46ea8ba2bffb47b38ad75c058afe
jbird55044 <jbird.m1@JamesDBird.com>
2020-12-03 13:26:55 -0600

    Updated DELETE Route both sides of fence

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,4 +5,19 @@ const pool = require('../modules/pool.js');
 // TODO - Add routes here...
 
 
-module.exports = router;
+router.delete('/:id', (req, res) => {
+    let reqId = req.params.id;
+    console.log('Delete request for id', reqId);
+    let sqlText = 'DELETE FROM groceries WHERE id=$1;';
+    pool.query(sqlText, [reqId])
+        .then((result) => {
+            console.log('Grocery Item deleted', reqId);
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+})
+
+module.exports = router; 
