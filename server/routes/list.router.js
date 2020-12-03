d9cbf25de5109218db698914ebd42394931a70de
kastenbauer27 <pkast2016@comcast.net>
2020-12-03 13:39:21 -0600

    completed get route on client and server, componentDidMount

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,17 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+router.get('/', (req, res) => {
+    const sqlQueryText = `SELECT * FROM groceries`;
+    pool.query(sqlQueryText)
+    .then( result => {
+        res.send(result.rows);
+    })
+    .catch(err => {
+        console.log('Error making GET request to database', error);
+        res.sendStatus(500);
+    })
+})
+
 
 module.exports = router;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    shoppingList: []
+  }
+
+  componentDidMount() {
+    this.getGroceries();
+  }
+
+  getGroceries = () => {
+    axios({
+      method: 'GET',
+      url: '/list',
+    }).then(res => {
+      console.log('here are all the groceries on the list', res.data);
+      this.setState({
+        shoppingList: [...this.state.shoppingList, res.data]
+      })
+    }).catch((error) => {
+      alert('Unable to get student list.');
+      console.log('Error in GET', error);
+    })
+  }
+
   render() {
+
+    console.log(this.state.shoppingList);
+
     return (
       <div className="App">
         <header className="banner-header">
