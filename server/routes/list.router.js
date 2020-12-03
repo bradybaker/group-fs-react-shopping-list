ce2765fc53426ad0648af96a0610af7b61b39f7e
Brady Baker <bradybaker82@gmail.com>
2020-12-03 13:36:44 -0600

    Post route made

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,18 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+router.post('/', (req, res) => {
+    const newGrocery = req.body
+    const sqlText = `INSERT INTO "groceries" ("item", "quantity", "unit") VALUES ($1, $2, $3);`
+    pool.query(sqlText, [newGrocery.item, newGrocery.quantity, newGrocery.unit])
+        .then((result) => {
+            console.log('Added new grocery to the database ->', newGrocery)
+            res.sendStatus(201)
+        }).catch((error) => {
+            console.log('Error in server POST', error)
+            res.sendStatus(500)
+        })
+})
+
 
 module.exports = router;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+
+  postGroceries = (newGrocery) => {
+    console.log('POSTING new grocery in client ->', newGrocery)
+    axios.post('/list', newGrocery)
+      .then((response) => {
+        console.log('POSTING in client', response)
+        this.getGroceries()
+      })
+      .catch((error) => {
+        console.log('Error in POSTING client', error)
+      })
+  }
+
   render() {
     return (
       <div className="App">
